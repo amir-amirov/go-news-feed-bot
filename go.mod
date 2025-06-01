315dd828a62c8d1d4f47dc4507fa9036beecd6ff
Amir Amirov <amir@Amirs-MacBook-Air.local>
2025-06-01 21:46:23 +0500

    Added notifier and markdown function

--- go.mod
+++ go.mod
@@ -4,11 +4,19 @@ go 1.24.1
 
 require (
 	github.com/SlyMarbo/rss v1.0.5 // indirect
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de // indirect
 	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
 	github.com/cristalhq/aconfig v0.18.7 // indirect
 	github.com/cristalhq/aconfig/aconfighcl v0.17.1 // indirect
+	github.com/go-shiori/dom v0.0.0-20230515143342-73569d674e1c // indirect
+	github.com/go-shiori/go-readability v0.0.0-20250217085726-9f5bf5ca7612 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/gogs/chardet v0.0.0-20211120154057-b7413eaefb8f // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	go.tomakado.io/containers v0.0.0-20240306123358-5f64d4e0f4f3 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
---
+++ internal/notifier/notifier.go
@@ -0,0 +1,122 @@
+package notifier
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/amir-amirov/go-news-feed-bot/internal/botkit/markup"
+	"github.com/amir-amirov/go-news-feed-bot/internal/model"
+	"github.com/go-shiori/go-readability"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type ArticlesRepository interface {
+	AllNotPosted(ctx context.Context, since time.Time, limit int) ([]model.Article, error)
+	MarkPosted(ctx context.Context, articleID int64) error
+}
+
+type Summarizer interface {
+	Summarizer(ctx context.Context, text string) (string, error)
+}
+
+type Notifier struct {
+	articlesRepository ArticlesRepository
+	summarizer         Summarizer
+	bot                *tgbotapi.BotAPI
+	sendInterval       time.Duration
+	lookupTimeWindow   time.Duration
+	channelID          int64
+}
+
+func New(articlesRepository ArticlesRepository, summarizer Summarizer, bot *tgbotapi.BotAPI, sendInterval, lookupTimeWindow time.Duration, channelID int64) *Notifier {
+	return &Notifier{
+		articlesRepository: articlesRepository,
+		summarizer:         summarizer,
+		bot:                bot,
+		sendInterval:       sendInterval,
+		lookupTimeWindow:   lookupTimeWindow,
+		channelID:          channelID,
+	}
+}
+
+func (n *Notifier) SelectAndSendArticle(ctx context.Context) error {
+	topOneArticles, err := n.articlesRepository.AllNotPosted(ctx, time.Now().Add(-n.lookupTimeWindow), 1)
+	if err != nil {
+		return fmt.Errorf("failed to fetch articles: %w", err)
+	}
+
+	if len(topOneArticles) == 0 {
+		return nil
+	}
+
+	article := topOneArticles[0]
+	summary, err := n.ExtractSummary(ctx, article)
+	if err != nil {
+		return fmt.Errorf("failed to extract summary: %w", err)
+	}
+
+	if err := n.sendArticle(article, summary); err != nil {
+		return fmt.Errorf("failed to send article: %w", err)
+	}
+
+	return n.articlesRepository.MarkPosted(ctx, article.ID)
+}
+
+func (n *Notifier) ExtractSummary(ctx context.Context, article model.Article) (string, error) {
+	var r io.Reader
+
+	if article.Summary != "" {
+		r = strings.NewReader(article.Summary)
+	} else {
+		response, err := http.Get(article.Link)
+		if err != nil {
+			return "", fmt.Errorf("article has no summary, tried to text from http get request but failed to fetch article content: %w", err)
+		}
+
+		defer response.Body.Close()
+
+		r = response.Body
+	}
+
+	doc, err := readability.FromReader(r, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse article content: %w", err)
+	}
+
+	summary, err := n.summarizer.Summarizer(ctx, clearText(doc.TextContent))
+
+	return "/n/n" + summary, nil
+}
+
+var redundantNewLines = regexp.MustCompile(`\n{3,}`)
+
+func clearText(text string) string {
+	return redundantNewLines.ReplaceAllString(text, "\n")
+}
+
+func (n *Notifier) sendArticle(article model.Article, summary string) error {
+	const msgFormat = "*%s*%s\n\n%s"
+
+	// Telegram requires escaping markdown characters
+	msg := tgbotapi.NewMessage(n.channelID, fmt.Sprintf(
+		msgFormat,
+		markup.EscapeForMarkdown(article.Title),
+		markup.EscapeForMarkdown(summary),
+		markup.EscapeForMarkdown(article.Link),
+	))
+
+	msg.ParseMode = tgbotapi.ModeMarkdownV2
+
+	_, err := n.bot.Send(msg)
+	if err != nil {
+		return fmt.Errorf("failed to send message: %w", err)
+	}
+
+	return nil
+}
